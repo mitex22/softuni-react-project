26ca5c35b9a2a4abda81663267b2ad21f6a1359a
mitex22 <mitex22@abv.bg>
2024-08-13 14:44:46 +0300

    Implement some APIs

---
+++ client/src/api/nfts-api.js
@@ -0,0 +1,56 @@
+import * as request from "./requester";
+
+const BASE_URL = 'http://localhost:3030/data/nfts';
+
+export const getAll = async () => {
+    const result = await request.get(BASE_URL);
+
+    const nfts = result;
+
+    return nfts;
+};
+
+export const getPortfolio = async (username) => {
+    const query = new URLSearchParams({
+        where: `_ownerUsername="${username}"`,
+        load: `nft=nftId:nfts`
+    });
+
+    const result = await request.get(`http://localhost:3030/data/portfolio?` + query);
+
+    const nfts = result;
+
+    return nfts;
+};
+
+export const getOne = (nftId) => request.get(`${BASE_URL}/${nftId}`);
+
+export const getLatest = async () => {
+    const result = await request.get(`${BASE_URL}?sortBy=_createdOn%20desc&pageSize=3`);
+
+    const nfts = result;
+
+    return nfts;
+}
+
+export const nftCreate = async (nftData) => {
+    const result = await request.post(BASE_URL, nftData);
+
+    return result;
+};
+
+export const nftEdit = async (nftId, nftData) => {
+    const result = await request.put(`${BASE_URL}/${nftId}`, nftData);
+
+    return result;
+};
+
+export const nftBuy = async (nftId, _ownerId, _ownerUsername) => {
+    const result = await request.post('http://localhost:3030/data/portfolio', { nftId, _ownerId, _ownerUsername });
+
+    return result;
+};
+
+export const nftSell = async (transactionId) => request.del(`http://localhost:3030/data/portfolio/${transactionId}`)
+
+export const nftDelete = async (nftId) => request.del(`${BASE_URL}/${nftId}`);
---
+++ client/src/api/requester.js
@@ -0,0 +1,45 @@
+async function requester(method, url, data) {
+    const options = {};
+
+    if (method !== 'GET') {
+        options.method = method;
+    }
+
+    if (data) {
+        options.headers = {
+            'Content-Type': 'application/json',
+        }   
+
+        options.body = JSON.stringify(data);
+    }
+
+    // local storage
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': accessToken
+        }
+    }
+
+    const response = await fetch(url, options);
+
+    if (response.status === 204) {
+        // return empty object due to server logout specifics
+        return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result;
+}
+
+export const get = requester.bind(null, 'GET');
+export const post = requester.bind(null, 'POST');
+export const put = requester.bind(null, 'PUT');
+export const del = requester.bind(null, 'DELETE');
