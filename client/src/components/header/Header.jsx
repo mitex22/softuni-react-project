2d6ce03d2ae4787d3f45af8f13f9dd64702d4c97
mitex22 <mitex22@abv.bg>
2024-08-13 17:00:27 +0300

    Implement Login Page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
         <>
             <nav className='bg-indigo-700 border-b border-indigo-500'>
                 <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-                    <div className='flex h-20 items-center justify-between'>
+                    <div className='flex h-[9.9vh] items-center justify-between'>
                         <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
                             <NavLink
                                 className='flex flex-shrink-0 items-center mr-4'
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -15,14 +15,14 @@ const Login = () => {
 
     // cleanup 
     useEffect(() => {
-        
+
         return () => {
             setError('');
         }
 
     }, []);
 
-    const { values, onChange, onSubmit} = useForm(loginSubmitHandler, { [LOGIN_FORM_KEYS.EMAIL]: '', [LOGIN_FORM_KEYS.PASSWORD]: '' });
+    const { values, onChange, onSubmit } = useForm(loginSubmitHandler, { [LOGIN_FORM_KEYS.EMAIL]: '', [LOGIN_FORM_KEYS.PASSWORD]: '' });
 
     const inputRef = useRef();
 
@@ -31,48 +31,60 @@ const Login = () => {
     }, []);
 
     return (
-        // <!-- Login Page ( Only for Guest users ) -->
-        <section id="login-page" className="auth">
-            <form id="login" onSubmit={onSubmit}>
+        <>
+            <section className="bg-gray-50 dark:bg-gray-300">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-[90vh] lg:py-0">
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Sign in to your account
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" action="#" onSubmit={onSubmit}>
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <input
+                                        ref={inputRef}
+                                        type="email"
+                                        id="email"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="name@company.com"
+                                        required=""
+                                        name={LOGIN_FORM_KEYS.EMAIL}
+                                        onChange={onChange}
+                                        value={values[LOGIN_FORM_KEYS.EMAIL]} />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        placeholder="••••••••"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required=""
+                                        name={LOGIN_FORM_KEYS.PASSWORD}
+                                        onChange={onChange}
+                                        value={values[LOGIN_FORM_KEYS.PASSWORD]}
+                                    />
+                                </div>
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input 
-                        ref={inputRef}
-                        type="email" 
-                        id="email" 
-                        name={LOGIN_FORM_KEYS.EMAIL} 
-                        placeholder="Sokka@gmail.com" 
-                        onChange={onChange}
-                        value={values[LOGIN_FORM_KEYS.EMAIL]}
-                    />
+                                <button type="submit" className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input 
-                        type="password" 
-                        id="login-password" 
-                        name={LOGIN_FORM_KEYS.PASSWORD} 
-                        onChange={onChange}
-                        value={values[LOGIN_FORM_KEYS.PASSWORD]}
-                    />
+                                {error &&
+                                    <p>
+                                        <span>{error}</span>
+                                    </p>
+                                }
 
-                    <input type="submit" className="btn submit" value="Login" />
-
-                    <p className="field">
-                        <span>If you don't have profile click <Link to={PATH.REGISTER}>here</Link></span>
-                    </p>
-
-                    {error && 
-                        <p>
-                            <span>{error}</span>
-                        </p>
-                    }
-                    
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet? <Link className="font-medium text-purple-600 dark:text-purple-500 hover:text-purple-700 " to={PATH.REGISTER}>Sign Up</Link>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </form>
-        </section>
+            </section>
+
+        </>
     )
 }
 
