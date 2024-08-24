57bae21a44b2e3ccece678fc478f9c147a7d676f
mitex22 <mitex22@abv.bg>
2024-08-24 23:04:33 +0300

    implement expand - collapse menu

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,10 +1,16 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import AuthContext from '../../contexts/authContext';
 import PATH from '../../paths/paths';
 import nftLogo from '../../assets/images/nft-logo.png';
 
 const Header = () => {
+
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const hamburgerHandler = () => {
+        setIsExpanded(!isExpanded);
+    }
 
     const {
         isAuthenticated,
@@ -26,17 +32,27 @@ const Header = () => {
                             <span className="text-slate-700 self-center text-xl text-gray-700 font-extrabold whitespace-nowrap">NFT Zoo</span>
                         </NavLink>
                         <div className="flex items-center">
-                            <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
+                            <button 
+                                data-collapse-toggle="mobile-menu-2" 
+                                type="button" 
+                                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" 
+                                aria-controls="mobile-menu-2" 
+                                aria-expanded="false"
+                                onClick={hamburgerHandler}>
                                 <span className="sr-only">Open main menu</span>
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                <svg className={`${isExpanded ? 'hidden ' : ''}w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                                <svg className={`${!isExpanded ? 'hidden ' : ''}w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </button>
                         </div>
 
-                        <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                        <div 
+                            className={`${!isExpanded ? 'hidden ' : ''}justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} 
+                            id="mobile-menu-2">
                             {/* User */}
                             {isAuthenticated && (
-                                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                <ul 
+                                    className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"
+                                    onClick={hamburgerHandler}>
 
                                     <li>
                                         <NavLink to={PATH.NFTs} className={linkClass}>All NFTs</NavLink>
@@ -58,7 +74,10 @@ const Header = () => {
 
                             {/* Guest */}
                             {!isAuthenticated && (
-                                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                <ul 
+                                    className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"
+                                    onClick={hamburgerHandler}
+                                    >
                                     <li>
                                         <NavLink to={PATH.LOGIN} className={linkClass}>Login</NavLink>
                                     </li>
