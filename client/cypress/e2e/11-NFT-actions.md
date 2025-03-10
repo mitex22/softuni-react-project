f823628a95c3fd24fe81cea355796bb520f886fa
mitex22 <mitex22@abv.bg>
2025-03-10 17:22:07 +0200

    add NFT actions scenarios

---
+++ client/cypress/e2e/11-NFT-actions.md
@@ -0,0 +1,49 @@
+1. **Buy and Sell NFT - Authenticated and Owner**:
+   - Log in with valid credentials (email: peter@abv.bg, password: 123456).
+   - Navigate to the "All NFTs" page.
+   - Search for an NFT by typing "Kondyo" in the search input.
+   - Click on the "Details" button for the NFT.
+   - Verify that the "Buy NFT" button is displayed.
+   - Click the "Buy NFT" button.
+   - Verify that a success message "Successfully bought Kondyo for 4 ETH!" is displayed.
+   - Verify that the user is navigated to "My NFT Collection" page.
+   - Verify that the NFT is successfully added to the portfolio by checking the portfolio list.
+   - Click the "Sell NFT" button for the NFT "Kondyo".
+   - Verify that the confirmation message "Are you sure you want to sell Kondyo?" is displayed.
+   - Cancel the sell action and verify that the NFT is still in the portfolio.
+   - Click the "Sell NFT" button for the NFT "Kondyo" again.
+   - Confirm the sell action and verify that a success message "Successfully sold Kondyo!" is displayed.
+   - Verify that the NFT is successfully removed from the portfolio by checking the portfolio list.
+   - Log out.
+
+2. **Delete NFT - Authenticated and Owner**:
+   - Log in with valid credentials (email: peter@abv.bg, password: 123456).
+   - Navigate to the "All NFTs" page.
+   - Search for an NFT by typing "Kondyo" in the search input.
+   - Click on the "Details" button for the NFT.
+   - Verify that the "Delete NFT" button is displayed.
+   - Click the "Delete NFT" button.
+   - Verify that the confirmation message "Are you sure you want to delete Kondyo? All of your data will be permanently removed. This action cannot be undone." is displayed.
+   - Cancel the delete action and verify that the NFT is still in the list.
+   - Click the "Delete NFT" button again.
+   - Confirm the delete action and verify that a success message "Successfully deleted Kondyo!" is displayed.
+   - Verify that the NFT is successfully removed from the list by checking the NFTs list.
+   - Log out.
+
+3. **Buy and Sell NFT - Authenticated but Not Owner**:
+   - Log in with valid credentials (email: george@abv.bg, password: 123456).
+   - Navigate to the "All NFTs" page.
+   - Search for an NFT by typing "Pirate" in the search input.
+   - Click on the "Details" button for the NFT.
+   - Verify that the "Buy NFT" button is displayed.
+   - Click the "Buy NFT" button.
+   - Verify that a success message "Successfully bought Pirate for 7 ETH!" is displayed.
+   - Verify that the user is navigated to "My NFT Collection" page.
+   - Verify that the NFT is successfully added to the portfolio by checking the portfolio list.
+   - Click the "Sell NFT" button for the NFT "Pirate".
+   - Verify that the confirmation message "Are you sure you want to sell Pirate?" is displayed.
+   - Cancel the sell action and verify that the NFT is still in the portfolio.
+   - Click the "Sell NFT" button for the NFT "Pirate" again.
+   - Confirm the sell action and verify that a success message "Successfully sold Pirate!" is displayed.
+   - Verify that the NFT is successfully removed from the portfolio by checking the portfolio list.
+   - Log out.
---
+++ client/cypress/e2e/11-NFT-actions.spec.js
@@ -0,0 +1,204 @@
+/// <reference types="cypress" />
+
+describe("NFT Comments Component Tests Suite", () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it("buy and sell nft - auth and owner", () => {
+        cy.get('nav').contains('Login').click();
+        cy.get('form input[type="email"]').type('peter@abv.bg', { delay: 100 });
+        cy.get('form input[type="password"]').type('123456', { delay: 100 });
+        cy.get('button').contains('Log In').click();
+
+        cy.get('nav').contains('All NFTs').click();
+        cy.get('input[placeholder="Search by NFT title..."]').type('Kondyo', { delay: 100 });
+        cy.get('a').contains('Details').click();
+
+        cy.get('button').contains('Buy NFT').should('exist');
+
+        cy.get('button').contains('Buy NFT').click();
+        cy.get('div').contains('Successfully bought Kondyo for 4 ETH!').should('exist');
+
+        // navigated to My Portfolio
+        cy.get('h1').contains('My NFT Collection').should('exist');
+
+        // check NFT bought successfully
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(4);
+        });
+
+        cy.get('h5').contains('Kondyo').parent().within(() => {
+            cy.get('button').contains('Sell NFT').click();
+        });
+
+        cy.get('p').contains('Are you sure you want to sell Kondyo?').should('exist');
+
+        // cancel delete
+        cy.get('button').contains('Cancel').click();
+
+        // check NFT sell cancelled
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(4);
+        });
+
+        cy.get('h5').contains('Kondyo').parent().within(() => {
+            cy.get('button').contains('Sell NFT').click();
+        });
+
+        // cancel sell
+        cy.get('button').contains('Cancel').click();
+
+        // check NFT sell cancelled
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(4);
+        });
+
+        cy.get('h5').contains('Kondyo').parent().within(() => {
+            cy.get('button').contains('Sell NFT').click();
+        });
+
+        // confirm sell
+        cy.get('h3').contains('Sell NFT').parent().parent().parent().parent().within(() => {
+            cy.get('button').contains('Sell').click();
+        });
+
+        cy.get('div').contains('Successfully sold Kondyo!').should('exist');
+
+        // check NFT sell successful
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(3);
+        });
+
+        cy.get('h5').contains('Kondyo').should('not.exist');
+
+        cy.get('nav').contains('Log Out').click();
+    });
+
+    it("delete nft - auth and owner", () => {
+        cy.get('nav').contains('Login').click();
+        cy.get('form input[type="email"]').type('peter@abv.bg', { delay: 100 });
+        cy.get('form input[type="password"]').type('123456', { delay: 100 });
+        cy.get('button').contains('Log In').click();
+
+        cy.get('nav').contains('All NFTs').click();
+        cy.get('input[placeholder="Search by NFT title..."]').type('Kondyo', { delay: 100 });
+        cy.get('a').contains('Details').click();
+
+        cy.get('button').contains('Delete NFT').should('exist');
+        cy.get('button').contains('Buy NFT').should('exist');
+
+        cy.get('button').contains('Delete NFT').click();
+
+        cy.get('h3').contains('Delete NFT').should('exist');
+        cy.get('p').contains('Are you sure you want to delete Kondyo? All of your data will be permanently removed. This action cannot be undone.').should('exist');
+
+        // cancel delete
+        cy.get('button').contains('Cancel').click();
+
+        // check NFT comment is NOT deleted
+        cy.request('GET', 'http://localhost:3030/data/nfts').then((response) => {
+            expect(response.body).to.have.length(12);
+            let isFound = false;
+            response.body.forEach((nft) => {
+                if (nft.title === 'Kondyo') {
+                    isFound = true;
+                }
+            });
+            expect(isFound).to.be.true;
+        });
+
+        cy.get('button').contains('Delete NFT').click();
+
+        // confirm delete
+        cy.get('h3').contains('Delete NFT').parent().parent().parent().parent().within(() => {
+            cy.get('button').contains('Delete').click();
+        });
+
+        cy.get('div').contains('Successfully deleted Kondyo!').should('exist');
+
+        // check NFT comment is deleted
+        cy.request('GET', 'http://localhost:3030/data/nfts').then((response) => {
+            expect(response.body).to.have.length(11);
+            let isFound = false;
+            response.body.forEach((nft) => {
+                if (nft.title === 'Kondyo') {
+                    isFound = true;
+                }
+            });
+            expect(isFound).to.be.false;
+        });
+
+        cy.get('nav').contains('Log Out').click();
+    });
+
+    it("buy and sell nft - auth but not owner", () => {
+        cy.get('nav').contains('Login').click();
+        cy.get('form input[type="email"]').type('george@abv.bg', { delay: 100 });
+        cy.get('form input[type="password"]').type('123456', { delay: 100 });
+        cy.get('button').contains('Log In').click();
+
+        cy.get('nav').contains('All NFTs').click();
+        cy.get('input[placeholder="Search by NFT title..."]').type('Pirate', { delay: 100 });
+        cy.get('a').contains('Details').click();
+
+        cy.get('button').contains('Buy NFT').should('exist');
+
+        cy.get('button').contains('Buy NFT').click();
+        cy.get('div').contains('Successfully bought Pirate for 7 ETH!').should('exist');
+
+        // navigated to My Portfolio
+        cy.get('h1').contains('My NFT Collection').should('exist');
+
+        // check NFT bought successfully
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(4);
+        });
+
+        cy.get('h5').contains('Pirate').parent().within(() => {
+            cy.get('button').contains('Sell NFT').click();
+        });
+
+        cy.get('p').contains('Are you sure you want to sell Pirate?').should('exist');
+
+        // cancel delete
+        cy.get('button').contains('Cancel').click();
+
+        // check NFT sell cancelled
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(4);
+        });
+
+        cy.get('h5').contains('Pirate').parent().within(() => {
+            cy.get('button').contains('Sell NFT').click();
+        });
+
+        // cancel sell
+        cy.get('button').contains('Cancel').click();
+
+        // check NFT sell cancelled
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(4);
+        });
+
+        cy.get('h5').contains('Pirate').parent().within(() => {
+            cy.get('button').contains('Sell NFT').click();
+        });
+
+        // confirm sell
+        cy.get('h3').contains('Sell NFT').parent().parent().parent().parent().within(() => {
+            cy.get('button').contains('Sell').click();
+        });
+
+        cy.get('div').contains('Successfully sold Pirate!').should('exist');
+
+        // check NFT sell successful
+        cy.request('GET', 'http://localhost:3030/data/portfolio').then((response) => {
+            expect(response.body).to.have.length(3);
+        });
+
+        cy.get('h5').contains('Pirate').should('not.exist');
+
+        cy.get('nav').contains('Log Out').click();
+    });
+});
